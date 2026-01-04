1f318942193e3a0378ab8b96cf211758b61b54c1
Julian Marvick Fua Oliveros <julian.marvick.oliveros@gmail.com>
2026-01-04 12:29:22 +0900

    Create mlops-dev namespace

--- setup/setup-argo.sh
+++ setup/setup-argo.sh
@@ -16,7 +16,10 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # 2. Expose ArgoCD server via NodePort
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 
-# 3. Get NodePort and initial password
+# 3. Create namespace for ArgoCD Destination
+kubectl create namespace mlops-dev || true
+
+# 4. Get NodePort and initial password
 NODE_PORT=$(kubectl get svc argocd-server -n argocd -o jsonpath='{.spec.ports[0].nodePort}')
 ADMIN_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
